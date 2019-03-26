411d022bac3497e05236cf0f6e942dd7ae896994
bummings <eabummings@gmail.com>
2019-03-25 23:57:51 -0400

    adds schema / fixes nodemon server.js specification

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const schema = require('./schema');
 
 const app = express();
 
 app.use(
   '/graphql',
   graphqlHTTP({
-    schema: spaceSchema,
-    graphql: true
+    schema,
+    graphiql: true
   })
 );
 
-const PORT = process.env.PORT || 6666;
+const PORT = 6666;
 
 app.listen(PORT, () => console.log(`Server be runnin on port ${PORT}`));
